cec7c9d6d13eeadb98335818850c6524d1bf7259
Souvik Maity <souvik1189.be21@chitkarauniveristy.edu.in>
2026-07-11 23:50:23 +0530

    Fix Socket.IO CORS

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://chitt-chatt-chatapp-1.onrender.com",
+    ],
+    credentials: true,
+    methods: ["GET", "POST"],
   },
 });
 
@@ -15,8 +20,7 @@ export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
+const userSocketMap = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -24,11 +28,9 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
